da1eacc54343e67f376708be02875f8cb9f1e375
Gael Hofman <Gieltie@outlook.fr>
2023-06-20 13:12:58 +0200

    made the highlight css when selected

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ import { catsData } from "./catsdata.js"
 
 const emotionRadio = document.getElementById('emotion-radios')
 
+emotionRadio.addEventListener('change', highlightCheckedOption)
+
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
