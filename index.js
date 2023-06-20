35730ddc1b3863407f6067f14d4d31642035bb70
Gael Hofman <Gieltie@outlook.fr>
2023-06-20 10:21:22 +0200

    used the ifnot includes so it wont duplicate

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if(!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
@@ -16,7 +18,15 @@ function renderEmotionsRadios(cats){
     let radioItem = ""
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItem += `<p>${emotion}</p>`
+        radioItem += `
+        <div class="radio">    
+            <label for="${emotion}">${emotion}</label>
+            <input
+                type="radio"
+                id="${emotion}"
+                value="${emotion}"
+                name="emotions">
+        </div>`
     }
     emotionRadio.innerHTML = radioItem
 }
