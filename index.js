3e80b9f99d12fdeed172fe7ad5d19ff10856483c
Gael Hofman <Gieltie@outlook.fr>
2023-06-20 08:40:29 +0200

    got the images

--- index.js
+++ index.js
@@ -169,3 +169,20 @@ const catsData = [
     },
 ]
 
+function getEmotionsArray(cats){
+    const emotionsArray = []
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            emotionsArray.push(emotion)
+        }
+    }
+    return emotionsArray
+}
+
+function renderEmotionsRadios(cats){
+    
+    const emotions = getEmotionsArray(cats)
+    console.log(emotions)
+}
+
+renderEmotionsRadios(catsData)
