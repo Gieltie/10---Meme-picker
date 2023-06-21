673a135335cef1f91668923620f7222e51c3a173
Gael Hofman <Gieltie@outlook.fr>
2023-06-21 09:15:57 +0200

    ordered the code and finished the project

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const memeModal = document.getElementById('meme-modal')
 const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
-getImageBtn.addEventListener('click', renderCat)
 memeModalCloseBtn.addEventListener('click', closeModal)
+getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -19,6 +19,31 @@ function highlightCheckedOption(e){
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+    <img 
+    class="cat-img" 
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+    >`
+    memeModal.style.display = 'flex'
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    if(catsArray.length === 1){
+        return (catsArray[0])
+    } else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return (catsArray[randomNumber])
+    }
+
+}
 
 function getMatchingCatsArray(){     
     if(document.querySelector('input[type="radio"]:checked')){
@@ -39,31 +64,6 @@ function getMatchingCatsArray(){
     }  
 }
 
-function getSingleCatObject(){
-    const catsArray = getMatchingCatsArray()
-    if(catsArray.length === 1){
-        return (catsArray[0])
-    } else {
-        const randomNumber = Math.floor(Math.random() * catsArray.length)
-        return (catsArray[randomNumber])
-    }
-
-}
-function renderCat(){
-    const catObject = getSingleCatObject()
-    memeModalInner.innerHTML = `
-    <img 
-        class="cat-img" 
-        src="./images/${catObject.image}"
-        alt="${catObject.alt}"
-    >`
-    memeModal.style.display = 'flex'
-}
-
-function closeModal(){
-    memeModal.style.display = 'none'
-}
-
 function getEmotionsArray(cats){
     const emotionsArray = []    
     for (let cat of cats){
@@ -75,7 +75,6 @@ function getEmotionsArray(cats){
     }
     return emotionsArray
 }
-
 
 function renderEmotionsRadios(cats){
     let radioItems = ``
